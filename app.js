18878e2105a0f0ebc0adf8abf265a3aa1ea8110a
Ira Clariz Lim <iraclarizl@gmail.com>
2024-03-01 02:25:01 +0800

    feat: I added navigations on the about section and also my information

--- app.js
+++ app.js
@@ -39,5 +39,20 @@ function PageTransitions() {
 
 PageTransitions();
 
+//About
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
 
 
