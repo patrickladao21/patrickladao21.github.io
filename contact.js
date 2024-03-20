6a213b8e74309d4082c1095afdc0e0145e7c0cb9
Ira Clariz Lim <iraclarizl@gmail.com>
2024-03-21 00:28:28 +0800

    feat: I finished the contact section

---
+++ contact.js
@@ -0,0 +1,29 @@
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_1fxdvic','template_gfvq2a9','#contact-form','OgiCyWLjVv8VauPNd')
+        .then(() => {
+            // Show sent message
+            contactMessage.textContent = 'Message sent successfully ✅'
+
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+
+            // Clear input fields
+            contactForm.reset()
+
+        },  () => {
+            // Show error message
+            contactMessage.textContent = 'Message not sent (service error) ❌'
+
+        })
+
+}
+
+contactForm.addEventListener('submit', sendEmail)
